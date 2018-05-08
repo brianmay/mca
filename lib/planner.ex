524aab31a324f001fcb77e16b80eea5fbe5bcbf4
Brian May <brian@linuxpenguins.xyz>
2018-05-08 19:08:22 +1000

    More development

--- lib/planner.ex
+++ lib/planner.ex
@@ -5,8 +5,8 @@ defmodule Planner do
     {:ok, %{"directions" => directions}} = Ptv.get_directions(route_id)
 
     directions
-    |> Enum.find(&(Map.get(&1, "direction_name") == "City (Flinders Street)"))
-    |> Map.get("direction_id")
+    |> Enum.find(&(Map.fetch!(&1, "direction_name") == "City (Flinders Street)"))
+    |> Map.fetch!("direction_id")
   end
 
   def get_departure(%{
@@ -16,17 +16,27 @@ defmodule Planner do
         :stop_id => stop_id,
         :date_utc => date_utc
       }) do
-    query = [
-      direction_id: direction_id,
-      date_utc: date_utc,
-      max_results: 1
-    ]
-
-    {:ok, %{"departures" => departures}} =
-      Ptv.get_departures(route_type, stop_id, route_id, query)
+    {:ok, %{"departures" => departures, "runs" => runs}} =
+      Ptv.get_departures(
+        route_type,
+        stop_id,
+        route_id,
+        direction_id: direction_id,
+        date_utc: date_utc,
+        max_results: 1,
+        expand: "run"
+      )
 
     [departure | _] = departures
-    {:ok, departure}
+    run_id = Map.fetch!(departure, "run_id")
+    run = Map.fetch!(runs, Integer.to_string(run_id))
+    {:ok, departure, run}
+  end
+
+  def format_datetime(datetime) do
+    datetime
+    |> Calendar.DateTime.shift_zone!(@time_zone)
+    |> Calendar.DateTime.Format.iso8601()
   end
 
   def parse_time(time_str) do
@@ -37,134 +47,204 @@ defmodule Planner do
     )
   end
 
-  def get_departure_details(%{"estimated_departure_utc" => departure_time})
+  def get_stop_dt(%{"estimated_departure_utc" => departure_time})
       when not is_nil(departure_time) do
     {true, Ptv.parse_datetime(departure_time)}
   end
 
-  def get_departure_details(%{"scheduled_departure_utc" => departure_time})
+  def get_stop_dt(%{"scheduled_departure_utc" => departure_time})
       when not is_nil(departure_time) do
     {false, Ptv.parse_datetime(departure_time)}
   end
 
-  def get_departure_details(_departure) do
+  def get_stop_dt(_departure) do
     raise "No time supplied in departure details"
   end
 
-  def print_departure_details(departure, message) do
-    {real_time, departure_dt} = get_departure_details(departure)
+  def print_stop_details(departure, message) do
+    {real_time, departure_dt} = get_stop_dt(departure)
 
     dt =
       departure_dt
       |> Calendar.DateTime.shift_zone!(@time_zone)
       |> Calendar.DateTime.Format.iso8601()
 
-    IO.puts("#{message} #{departure["stop_id"]} #{real_time} #{dt}")
+    IO.puts("#{message} #{Map.fetch!(departure, "stop_id")} #{real_time} #{dt}")
   end
 
   def get_stop_from_pattern(pattern, stop_id) do
-    pattern
-    |> Enum.find(&(Map.get(&1, "stop_id") == stop_id))
+    result =
+      pattern
+      |> Enum.find(&(Map.fetch!(&1, "stop_id") == stop_id))
+
+    %{} = result
   end
 
-  def process_change_connection_unique_run(change, stop, connection, runs) do
-    print_departure_details(connection, "Connection")
+  def get_prev_stop(pattern, stop_id) do
+    index = Enum.find_index(pattern, &(Map.fetch!(&1, "stop_id") == stop_id))
 
-    {_, stop_datetime} = get_departure_details(stop)
-    {_, depart_datetime} = get_departure_details(connection)
+    if index <= 0 do
+      raise "Cannot get stop before first stop"
+    end
 
-    run_id = connection["run_id"]
+    Enum.at(pattern, index - 1)
+  end
 
-    run = runs[Integer.to_string(run_id)]
-    route_type = run["route_type"]
+  def get_next_stop(pattern, stop_id) do
+    index = Enum.find_index(pattern, &(Map.fetch!(&1, "stop_id") == stop_id))
 
-    if Calendar.DateTime.after?(depart_datetime, stop_datetime) do
-      {:ok, %{"departures" => pattern}} =
-        Ptv.get_pattern(run_id, route_type, date_utc: depart_datetime)
+    if index >= length(pattern) do
+      raise "Cannot get stop before first stop"
+    end
 
-      IO.puts(inspect(pattern))
+    Enum.at(pattern, index + 1)
+  end
 
-      # FIXME: 1071 shouldn't be hardcoded.
-      destination =
-        pattern
-        |> Enum.find(&(Map.get(&1, "stop_id") == 1071))
+  def estimate_arrival_time(pattern, stop_id) do
+    times = %{
+      # Richmond -> Flinders Street Station
+      {1162, 1071} => 4 * 60,
+      # Parliament -> Flinders Street Station
+      {1155, 1071} => 4 * 60,
+      # Flagstaff -> Flinders Street Station
+      {1181, 1071} => 4 * 60
+    }
 
-      # Something went wrong if destination isn't in stopping pattern.
-      %{} = destination
+    prev_stop = get_prev_stop(pattern, stop_id)
+    prev_stop_id = Map.fetch!(prev_stop, "stop_id")
+    {_, datetime} = get_stop_dt(prev_stop)
+    add_seconds = Map.get(times, {prev_stop_id, stop_id})
 
-      stops = Enum.map(pattern, & &1["stop_id"])
-      IO.puts(inspect(stops))
-      direct_sequence = change[:direct_sequence]
-      IO.puts(inspect(direct_sequence))
-      [:ok]
-    else
-      []
+    if is_nil(add_seconds) do
+      raise "No entry for {prev_stop_id} to {stop_id}"
+    end
+
+    Calendar.DateTime.add!(datetime, add_seconds)
+  end
+
+  def process_change_connection_pattern(pattern) do
+    # {_, stop_datetime} = get_stop_dt(stop)
+    # {_, depart_datetime} = get_stop_dt(departure)
+
+    # route_type = Map.fetch!(run, "route_type")
+
+    # FIXME: 1071 shouldn't be hardcoded.
+    destination_stop_id = 1071
+
+    last_stop = get_stop_from_pattern(pattern, destination_stop_id)
+
+    arrival_time = estimate_arrival_time(pattern, destination_stop_id)
+
+    %{
+      # connection: connection,
+      # stop: stop,
+      # run: run,
+      service2_pattern: pattern,
+      service2_last_stop: last_stop,
+      service2_arrival_time: arrival_time
+    }
+  end
+
+  def process_change_connection(arrive_dt, departure, runs, processed_runs) do
+    print_stop_details(departure, "Connection")
+    {_, depart_dt} = get_stop_dt(departure)
+
+    run_id = Map.fetch!(departure, "run_id")
+    run = Map.fetch!(runs, Integer.to_string(run_id))
+    route_type = Map.fetch!(run, "route_type")
+
+    cond do
+      Calendar.DateTime.before?(depart_dt, arrive_dt) ->
+        IO.puts("--> Connection ignored as too early.")
+        {processed_runs, []}
+
+      MapSet.member?(processed_runs, run_id) ->
+        IO.puts("---> Connection ignored as we have seen run #{run_id} already.")
+        {processed_runs, []}
+
+      true ->
+        IO.puts("---> Processing run #{run_id}.")
+        processed_runs = MapSet.put(processed_runs, run_id)
+
+        {:ok, %{"departures" => pattern}} =
+          Ptv.get_pattern(run_id, route_type, date_utc: depart_dt)
+
+        result =
+          Map.merge(process_change_connection_pattern(pattern), %{
+            service2_run: run,
+            service2_first_stop: departure
+          })
+
+        {processed_runs, [result]}
     end
   end
 
-  def process_change_connection(change, stop, connection, runs, processed_runs) do
-    run_id = connection["run_id"]
-
-    if not MapSet.member?(processed_runs, run_id) do
-      processed_runs = MapSet.put(processed_runs, run_id)
-      {processed_runs, process_change_connection_unique_run(change, stop, connection, runs)}
-    else
-      {processed_runs, []}
-    end
-  end
-
-  def process_change_connections(change, stop, [connection | connections], runs, processed_runs) do
+  def process_change_connections(arrive_dt, [departure | departures], runs, processed_runs) do
     {processed_runs, head_results} =
-      process_change_connection(change, stop, connection, runs, processed_runs)
+      process_change_connection(arrive_dt, departure, runs, processed_runs)
 
     {processed_runs, tail_results} =
-      process_change_connections(change, stop, connections, runs, processed_runs)
+      process_change_connections(arrive_dt, departures, runs, processed_runs)
 
     {processed_runs, head_results ++ tail_results}
   end
 
-  def process_change_connections(_change, _stop, [], _runs, processed_runs) do
+  def process_change_connections(_arrive_dt, [], _runs, processed_runs) do
     {processed_runs, []}
   end
 
-  def process_change_stop(change, stop, processed_runs) do
-    print_departure_details(stop, "Change stop")
-    {_real_time, departure_dt} = get_departure_details(stop)
+  def process_change_stop(change, service1_last_stop, processed_runs) do
+    print_stop_details(service1_last_stop, "Change stop")
+    {_real_time, arrive_dt} = get_stop_dt(service1_last_stop)
 
-    route_type = change[:route_type]
-    params = change[:params]
-    stop_id = stop["stop_id"]
+    route_type = change.route_type
+    params = change.params
+    stop_id = Map.fetch!(service1_last_stop, "stop_id")
 
     query =
       Keyword.merge(
         params,
         expand: "run",
         max_results: 10,
-        date_utc: departure_dt
+        date_utc: arrive_dt
       )
 
     {:ok, data} = Ptv.get_departures(route_type, stop_id, nil, query)
 
-    process_change_connections(change, stop, data["departures"], data["runs"], processed_runs)
+    process_change_connections(
+      arrive_dt,
+      Map.fetch!(data, "departures"),
+      Map.fetch!(data, "runs"),
+      processed_runs
+    )
   end
 
-  def process_change(pattern, change, processed_runs) do
-    stop_id = change[:stop_id]
+  def process_change(service1_pattern, change, processed_runs) do
+    stop_id = change.stop_id
+    service1_last_stop = get_stop_from_pattern(service1_pattern, stop_id)
 
-    stop = get_stop_from_pattern(pattern, stop_id)
+    extra_data = %{
+      service1_pattern: service1_pattern,
+      service1_last_stop: service1_last_stop
+    }
 
-    case stop do
-      nil -> []
-      _ -> process_change_stop(change, stop, processed_runs)
-    end
+    {processed_runs, results} =
+      case service1_last_stop do
+        nil -> {processed_runs, []}
+        _ -> process_change_stop(change, service1_last_stop, processed_runs)
+      end
+
+    results = Enum.map(results, &Map.merge(&1, extra_data))
+
+    {processed_runs, results}
   end
 
-  def process_changes(pattern, [change | changes], processed_runs) do
-    {processed_runs, results} = process_change(pattern, change, processed_runs)
-    results ++ process_changes(pattern, changes, processed_runs)
+  def process_changes(service1_pattern, [change | changes], processed_runs) do
+    {processed_runs, results} = process_change(service1_pattern, change, processed_runs)
+    results ++ process_changes(service1_pattern, changes, processed_runs)
   end
 
-  def process_changes(_pattern, [], _processed_runs) do
+  def process_changes(_service1_pattern, [], _processed_runs) do
     []
   end
 
@@ -183,9 +263,9 @@ defmodule Planner do
       %{
         route_type: 0,
         stop_id: 1162,
-        direct_sequence: [1162, 1071],
-        direct_time: 4 * 60,
-        loop_time: 14 * 60,
+        # direct_next_stop: 1071,
+        # direct_time: 4 * 60,
+        # loop_time: 14 * 60,
         params: [
           direction_id: direction_id
         ]
@@ -193,16 +273,16 @@ defmodule Planner do
       %{
         route_type: 0,
         stop_id: 1155,
-        direct_sequence: [1155, 1071],
-        direct_time: 4 * 60,
-        loop_time: 10 * 60,
+        # direct_next_stop: 1071,
+        # direct_time: 4 * 60,
+        # loop_time: 10 * 60,
         params: [
           platform_numbers: 3
         ]
       }
     ]
 
-    {:ok, departure} =
+    {:ok, service1_first_stop, _service1_run} =
       get_departure(%{
         direction_id: direction_id,
         route_type: route_type,
@@ -211,13 +291,41 @@ defmodule Planner do
         date_utc: date_utc
       })
 
-    {_real_time, departure_dt} = get_departure_details(departure)
+    {_real_time, service1_start_dt} = get_stop_dt(service1_first_stop)
 
-    print_departure_details(departure, "Start")
+    print_stop_details(service1_first_stop, "Start")
 
-    {:ok, %{"departures" => pattern}} =
-      Ptv.get_pattern(departure["run_id"], route_type, date_utc: departure_dt)
+    {:ok, %{"departures" => service1_pattern}} =
+      Ptv.get_pattern(
+        Map.fetch!(service1_first_stop, "run_id"),
+        route_type,
+        date_utc: service1_start_dt
+      )
 
-    process_changes(pattern, changes, %MapSet{})
+    results = process_changes(service1_pattern, changes, %MapSet{})
+
+    results =
+      Enum.sort(results, fn x, y ->
+        Calendar.DateTime.before?(x.service2_arrival_time, y.service2_arrival_time)
+      end)
+
+    Enum.each(results, fn result ->
+      # {s1_first_real_time, s1_first_dt} = get_stop_dt(result.service1_last_stop)
+      {s1_last_real_time, s1_last_dt} = get_stop_dt(result.service1_last_stop)
+      {s2_first_real_time, s2_first_dt} = get_stop_dt(result.service2_first_stop)
+      s2_last_dt = result.service2_arrival_time
+
+      # stop_1 = service1_first_stop
+      stop_2 = result.service2_first_stop
+      platform = Map.fetch!(stop_2, "platform_number")
+      stop_3 = result.service2_last_stop
+
+      IO.puts(
+        "#{Map.fetch!(stop_2, "stop_id")} " <>
+          "#{format_datetime(s1_last_dt)} #{s1_last_real_time} " <>
+          "#{format_datetime(s2_first_dt)} #{s2_first_real_time} " <>
+          "#{platform} #{format_datetime(s2_last_dt)} #{Map.fetch!(stop_3, "stop_id")}"
+      )
+    end)
   end
 end
